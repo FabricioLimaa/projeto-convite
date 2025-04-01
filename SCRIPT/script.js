272630bc5ce2243ef8de86411d6bfe04e3a3f980
Fabricio Lima <fabriciodacostalima@gmail.com>
2025-03-31 22:29:10 -0300

    EmailJS

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
         });
   
         try {
-            const response = await emailjs.send("service_kk4qlks", "template_mq5aj3i", data);
+            const response = await emailjs.send("service_kk4qlks", "template_s57h6ui", data);
   
             if (response.status === 200) {
                 mensagemDiv.text("Confirmação enviada com sucesso!").css("color", "green");
